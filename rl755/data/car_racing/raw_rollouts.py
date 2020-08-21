8b8da5e4970ccc43d887b4dfdf0d724c3c85d249
Michael Matena <michael.matena@gmail.com>
2020-08-21 10:37:37 -0400

    Default commit message.

--- rl755/data/car_racing/raw_rollouts.py
+++ rl755/data/car_racing/raw_rollouts.py
@@ -21,7 +21,7 @@ def get_raw_rollouts_ds(shuffle_files=True):
     x = {k: tf.sparse.to_dense(v) for k, v in x.items()}
 
     observations = tf.squeeze(x['observations'])
-    observations = tf.unstack(observations)
+    # observations = tf.unstack(observations)
     observations = [tf.io.parse_tensor(obs, out_type=tf.uint8) for obs in observations]
     x['observations'] = tf.stack(observations)
 

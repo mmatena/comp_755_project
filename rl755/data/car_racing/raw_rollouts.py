1b9651911679d6dd9bd4633046ea355c9d073a6f
Michael Matena <michael.matena@gmail.com>
2020-08-21 10:27:10 -0400

    Default commit message.

--- rl755/data/car_racing/raw_rollouts.py
+++ rl755/data/car_racing/raw_rollouts.py
@@ -19,6 +19,7 @@ def get_raw_rollouts_ds(shuffle_files=True):
     }
     _, x = tf.io.parse_single_sequence_example(x, sequence_features=features)
     x = {k: tf.sparse.to_dense(v) for k, v in x.items()}
+    x['observations'] = tf.io.parse_tensor(x['observations'])
     return x
 
   ds = tf.data.TFRecordDataset(files)

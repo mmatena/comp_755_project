9ad7fc7b6640397c9a93c1f03ec7f8bb7b0fd6f7
Michael Matena <michael.matena@gmail.com>
2020-08-21 12:55:00 -0400

    Default commit message.

--- rl755/data/car_racing/raw_rollouts.py
+++ rl755/data/car_racing/raw_rollouts.py
@@ -57,3 +57,15 @@ def random_rollout_slices(slice_size, shuffle_files=True):
 
   ds = get_raw_rollouts_ds(shuffle_files=shuffle_files)
   return ds.map(slice_example, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+
+def random_rollout_observations(shuffle_files=True):
+  # TODO(mmatena): Add docs.
+
+  def random_obs(x):
+    rollout_length = tf.shape(x['observations'])[0]
+    index = tf.random.uniform([], 0, rollout_length, dtype=tf.int32)
+    return {"observation": x['observations'][index]}
+
+  ds = get_raw_rollouts_ds(shuffle_files=shuffle_files)
+  return ds.map(random_obs, num_parallel_calls=tf.data.experimental.AUTOTUNE)

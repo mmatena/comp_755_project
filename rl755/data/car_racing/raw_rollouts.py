76e5d9a194cf94e547b2ba73a28899af13eeae4f
Michael Matena <michael.matena@gmail.com>
2020-08-21 10:54:42 -0400

    Default commit message.

--- rl755/data/car_racing/raw_rollouts.py
+++ rl755/data/car_racing/raw_rollouts.py
@@ -7,6 +7,18 @@ _TFRECORDS_PATTERN = "/pine/scr/m/m/mmatena/comp_755_project/data/car_racing/" \
 
 
 def get_raw_rollouts_ds(shuffle_files=True):
+  """Returns a tf.data.Dataset where each item is a raw full rollout.
+
+  Each example is a dict with items:
+    'observations': tf.uint8, (rollout_len, 96, 96, 3)
+    'actions': tf.float32, (rollout_len, 4)
+    'rewards': tf.float32, (rollout_len, 1)
+
+  Getting the i-th item in each of the tensors will provide values for the
+  observation on the i-th step, the action taken at the i-th step, and the
+  reward corresponding to the transition from the i-th state to the (i+1)-th
+  state.
+  """
   files = tf.io.matching_files(_TFRECORDS_PATTERN)
   if shuffle_files:
     files = tf.random.shuffle(files)
@@ -26,8 +38,3 @@ def get_raw_rollouts_ds(shuffle_files=True):
 
   ds = tf.data.TFRecordDataset(files)
   return ds.map(parse_fn, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-
-for x in get_raw_rollouts_ds():
-  print(x)
-  break

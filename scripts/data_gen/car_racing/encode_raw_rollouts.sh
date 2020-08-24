c56f5272b97281c956b66ea335193e24dd01ceef
Michael Matena <michael.matena@gmail.com>
2020-08-24 12:01:57 -0400

    Default commit message.

--- scripts/data_gen/car_racing/encode_raw_rollouts.sh
+++ scripts/data_gen/car_racing/encode_raw_rollouts.sh
@@ -19,7 +19,7 @@ MODEL="raw_rollout_vae_32ld"
 NUM_GPUS=4
 NUM_CORES=12
 # Needs high memory due as full rollouts are large.
-MEMORY=30g
+MEMORY=20g
 TIME="8:30:00"
 #############################################################
 

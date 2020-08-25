a1b81934b907c8916d7c3f2a606c18189644dbe5
Michael Matena <michael.matena@gmail.com>
2020-08-25 12:31:33 -0400

    Default commit message.

--- scripts/one_offs/vae_serialize_samples.py
+++ scripts/one_offs/vae_serialize_samples.py
@@ -14,12 +14,11 @@ flags.DEFINE_integer('num_samples', 1,
                      'The number of times to sample from the VAE.',
                      lower_bound=1)
 
-flags.mark_flag_as_required('num_samples')
-
 
 def main(_):
   vae = saved_models.raw_rollout_vae_32ld()
   z = vae.sample_unconditionally(FLAGS.num_samples)
+  z = tf.cast(255.0 * z, tf.unit8)
   serial = tf.io.serialize_tensor(z)
   print(serial.numpy())
 

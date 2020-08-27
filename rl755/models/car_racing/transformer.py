ee67cec66f226373595b6b5c4dc9259bf52e9c67
Michael Matena <michael.matena@gmail.com>
2020-08-27 17:22:38 -0400

    something

--- rl755/models/car_racing/transformer.py
+++ rl755/models/car_racing/transformer.py
@@ -1,1 +1,11 @@
 """Car racing specific transformer code."""
+import tensorflow as tf
+
+from rl755.models.common import transformer as common_transformer
+
+
+class AutoregressiveFwdOr(common_transformer.AutoregressiveTransformer):
+    # def __init__(self, transformer_params, output_size, **kwargs):
+    #     # TODO(mmatena): Add docs
+    #     super().__init__(transformer_params=transformer_params, output_size=output_size, **kwargs)
+    pass

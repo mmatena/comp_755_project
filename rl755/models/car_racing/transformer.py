b2eeba5391f8bc5072d1c48f64256189452818ce
Michael Matena <michael.matena@gmail.com>
2020-08-28 13:34:15 -0400

    Some refactoring and documentation

--- rl755/models/car_racing/transformer.py
+++ rl755/models/car_racing/transformer.py
@@ -4,8 +4,52 @@ import tensorflow as tf
 from rl755.models.common import transformer as common_transformer
 
 
+# TODO(mmatena): Put some special stuff here.
 class AutoregressiveFwdOr(common_transformer.AutoregressiveTransformer):
-    # def __init__(self, transformer_params, output_size, **kwargs):
-    #     # TODO(mmatena): Add docs
-    #     super().__init__(transformer_params=transformer_params, output_size=output_size, **kwargs)
     pass
+
+
+def ignore_prefix_loss(loss_fn, prefix_size):
+    """Ignore the losses on the first few tokens.
+
+    The idea is that the model might not have enough information to accurately
+    predict the next frame when modeling autoregressively. One reason is that you
+    cannot tell the velocity from a single frame.
+
+    Args:
+        loss_fn: a loss function compatible with keras
+        prefix_size: a non-negative integer, the loss on the first `prefix_size` tokens
+            will be ignored
+    Returns:
+        A loss function compatible with keras.
+    """
+
+    def fn(y_true, y_pred):
+        # We assume that the sequence dimension is the second dimension.
+        y_true = y_true[:, prefix_size:]
+        y_pred = y_pred[:, prefix_size:]
+        return loss_fn(y_true, y_pred)
+
+    return fn
+
+
+def to_ar_inputs_and_targets(x, sequence_length, latent_size=32, action_size=4):
+    """Given a slice of a rollout, convert it to inputs and targets for autoregressive modelling.
+
+    We do it like this:
+        o[i], a[i], r[i-1] => o[i+1], r[i]
+    The goal is to predict the next observation and reward given the action taken and the previous states
+    and rewards.
+    """
+    # TODO(mmatena): Make sure this is correct!
+    r = tf.expand_dims(x["rewards"], axis=-1)
+    a = x["actions"]
+    o = x["observations"]
+    inputs = tf.concat(
+        [o[:-1], a[:-1], tf.concat([[[0.0]], r[:-2]], axis=0)],
+        axis=-1,
+    )
+    targets = tf.concat([o[1:], r[:-1]], axis=-1)
+    inputs = tf.reshape(inputs, [sequence_length, latent_size + action_size + 1])
+    targets = tf.reshape(targets, [sequence_length, latent_size + 1])
+    return inputs, targets
--- scripts/one_offs/transformer_eval.py
+++ scripts/one_offs/transformer_eval.py
@@ -1,9 +1,13 @@
+"""Evaluate an autoregressive transformer on a validation set."""
+import functools
+
 from absl import app
 from absl import flags
 import tensorflow as tf
 
 from rl755.data.car_racing import encoded_rollouts
 from rl755.models.car_racing import saved_models
+from rl755.models.car_racing import transformer
 
 FLAGS = flags.FLAGS
 
@@ -11,44 +15,21 @@ flags.DEFINE_integer(
     "num_examples", 512, "The number of examples to evaluate on.", lower_bound=1
 )
 flags.DEFINE_integer("batch_size", 128, "Batch size for eval.", lower_bound=1)
-flags.DEFINE_integer("sequence_length", 32, "Size of windows for eval.", lower_bound=1)
 
-
-def ignore_prefix_loss(loss_fn, prefix_size):
-    def fn(y_true, y_pred):
-        # We assume that the sequence dimension is the second dimension.
-        y_true = y_true[:, prefix_size:]
-        y_pred = y_pred[:, prefix_size:]
-        return loss_fn(y_true, y_pred)
-
-    return fn
-
-
-def _to_inputs_and_targets(x):
-    # o[i], a[i], r[i-1] => o[i+1], r[i]
-    # TODO(mmatena): Make sure this is correct!
-    r = tf.expand_dims(x["rewards"], axis=-1)
-    a = x["actions"]
-    o = x["observations"]
-    inputs = tf.concat(
-        [o[:-1], a[:-1], tf.concat([[[0.0]], r[:-2]], axis=0)],
-        axis=-1,
-    )
-    targets = tf.concat([o[1:], r[:-1]], axis=-1)
-    # TODO(mmatena): Don't hardcode these shapes
-    inputs = tf.reshape(inputs, [FLAGS.sequence_length, 32 + 4 + 1])
-    targets = tf.reshape(targets, [FLAGS.sequence_length, 32 + 1])
-    return inputs, targets
+SEQUENCE_LENGTH = 32
 
 
 def get_ds():
     # NOTE: This won't be deterministic.
     ds = encoded_rollouts.random_rollout_slices(
-        slice_size=FLAGS.sequence_length + 1, split="validation"
+        slice_size=SEQUENCE_LENGTH + 1, split="validation"
     )
     ds = ds.take(FLAGS.num_examples)
     ds = ds.map(
-        _to_inputs_and_targets, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        functools.partial(
+            transformer.to_ar_inputs_and_targets, sequence_length=FLAGS.sequence_length
+        ),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE,
     )
     ds = ds.batch(FLAGS.batch_size)
     return ds
@@ -57,7 +38,9 @@ def get_ds():
 def main(_):
     ds = get_ds()
 
-    loss_fn = ignore_prefix_loss(tf.keras.losses.MeanSquaredError(), prefix_size=4)
+    loss_fn = transformer.ignore_prefix_loss(
+        tf.keras.losses.MeanSquaredError(), prefix_size=4
+    )
 
     model = saved_models.encoded_rollout_transformer()
     model.compile(optimizer="adam", loss=loss_fn)

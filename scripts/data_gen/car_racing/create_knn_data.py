714e34ad4eb069246286a55ec3eb26df20053c13
Michael Matena <michael.matena@gmail.com>
2020-08-30 11:35:30 -0400

    Default commit message.

--- scripts/data_gen/car_racing/create_knn_data.py
+++ scripts/data_gen/car_racing/create_knn_data.py
@@ -63,7 +63,7 @@ SEQUENCE_LENGTH = 32
 
 def get_model():
     model = saved_models.encoded_rollout_transformer()
-    model.build(input_shape=[FLAGS.batch_size, SEQUENCE_LENGTH, 32])
+    model.build(input_shape=[FLAGS.batch_size, SEQUENCE_LENGTH, 32 + 4 + 1])
     model.compile()
     return model
 

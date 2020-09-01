0c9d118b4b9286dddb83eb8cc8b24e67294b22bd
Michael Matena <michael.matena@gmail.com>
2020-09-01 11:36:28 -0400

    Default commit message.

--- scripts/one_offs/transformer_eval.sh
+++ scripts/one_offs/transformer_eval.sh
@@ -17,17 +17,27 @@ module add tensorflow_py3/2.1.0
 export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR
 
 run_python() {
-  echo python $PROJECT_DIR/scripts/one_offs/transformer_eval.py
+  # echo python $PROJECT_DIR/scripts/one_offs/transformer_eval.py
+  echo python pdb $PROJECT_DIR/scripts/one_offs/transformer_eval.py
 }
 
 run_singularity() {
-  echo singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME bash -c "\\\"$(run_python)\\\""
+  # echo singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME bash -c "\\\"$(run_python)\\\""
+  echo singularity shell --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME bash -c "\\\"$(run_python)\\\""
 }
 
 launch() {
   # Not too sure why I have to do it like this, but just running the command
   # causes it fail to launch.
-  CMD=$(echo sbatch \
+  # CMD=$(echo sbatch \
+  #   --ntasks=1 \
+  #   --time=0:30:00 \
+  #   --mem=6g \
+  #   --partition=volta-gpu \
+  #   --gres=gpu:1 \
+  #   --qos=gpu_access \
+  #   --wrap="\"$(run_singularity)\"")
+  CMD=$(echo srun \
     --ntasks=1 \
     --time=0:30:00 \
     --mem=6g \

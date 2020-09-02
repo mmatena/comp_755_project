fea68c412d8088664cbfd8cb4fbfaa53b22e1ec5
Michael Matena <michael.matena@gmail.com>
2020-09-02 18:31:15 -0400

    Default commit message.

--- rl755/models/common/transformer.py
+++ rl755/models/common/transformer.py
@@ -146,12 +146,16 @@ class AutoregressiveLookupTransformer(tf.keras.Model):
         queries = self.get_queries(layers_with_output)
         values, distances = self.lookup_layer(queries)
 
+        # Need to explicitly set the shapes or else the mod
         values = tf.reshape(
             values,
-            tf.concat([tf.shape(outputs)[:2], [10, tf.shape(outputs)[-1]]], axis=0),
+            tf.concat(
+                [tf.shape(outputs)[:2], [self.knn_lookup.k, tf.shape(outputs)[-1]]],
+                axis=0,
+            ),
         )
         distances = tf.reshape(
-            distances, tf.concat([tf.shape(outputs)[:2], [10]], axis=0)
+            distances, tf.concat([tf.shape(outputs)[:2], [self.knn_lookup.k]], axis=0)
         )
 
         weights = tf.nn.softmax(distances)

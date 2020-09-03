bc938bcaf1132bcf8d20ea5c38d07ab7e9e5861c
Michael Matena <michael.matena@gmail.com>
2020-09-02 21:07:34 -0400

    Default commit message.

--- scripts/one_offs/transformer_eval.py
+++ scripts/one_offs/transformer_eval.py
@@ -57,8 +57,6 @@ def get_ds():
 
 
 def main(_):
-    if True:
-        raise ValueError("TRY EVALUATING ON THE TRAIN SET!")
     gpus = tf.config.list_physical_devices("GPU")
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)

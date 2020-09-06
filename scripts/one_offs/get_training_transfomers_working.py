b5663e53d8643aa3e2951fc8dee2d90c30a166cd
Michael Matena <michael.matena@gmail.com>
2020-09-06 17:32:41 -0400

    Default commit message.

--- scripts/one_offs/get_training_transfomers_working.py
+++ scripts/one_offs/get_training_transfomers_working.py
@@ -17,9 +17,10 @@ from rl755.models.common import transformer as common_transformer
 hidden_size = 4
 num_attention_heads = 1
 transformer_params = TransformerEncoderLayer.Params(
-    num_layers=2,
+    num_layers=1,
     hidden_size=hidden_size,
     hidden_dropout=0.0,
+    attention_dropout=0.0,
     intermediate_size=4 * hidden_size,
     intermediate_activation=None,
     num_heads=num_attention_heads,
@@ -35,6 +36,37 @@ model = tf.keras.models.Sequential(
         # tf.keras.layers.Dense(hidden_size, activation=None)
     ]
 )
+"""
+num_layers     = None
+out_layer_ndxs = None   # [-1]
+
+intermediate_size       = None
+intermediate_activation = "gelu"
+
+hidden_size        = None
+hidden_dropout     = 0.1
+initializer_range  = 0.02
+adapter_size       = None       # bottleneck size of the adapter - arXiv:1902.00751
+adapter_activation = "gelu"
+adapter_init_scale = 1e-3
+
+initializer_range = 0.02
+
+hidden_size         = None
+num_heads           = None
+hidden_dropout      = None
+attention_dropout   = 0.1
+initializer_range   = 0.02
+
+num_heads         = None
+size_per_head     = None
+initializer_range = 0.02
+query_activation  = None
+key_activation    = None
+value_activation  = None
+attention_dropout = 0.1
+negative_infinity = -10000.0  # used for attention scores before softmax
+"""
 
 
 def gen():

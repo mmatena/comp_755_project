6f2aaecbdacabb0b0ee4cb988bd55e51c111b4d0
Michael Matena <michael.matena@gmail.com>
2020-09-06 12:41:04 -0400

    Default commit message.

--- rl755/models/car_racing/saved_models.py
+++ rl755/models/car_racing/saved_models.py
@@ -54,9 +54,11 @@ def encoded_rollout_transformer():
     # TODO(mmatena): Add docs explaing all the parameters this was trained with.
     # weights_path = "/pine/scr/m/m/mmatena/mog_ar_transformer_train/model.hdf5"
     weights_path = "/pine/scr/m/m/mmatena/test_mog_ar_transformer_train/model.hdf5"
-    # print("WARNING: WRONG WEIGHTS PATH!!!")
+    print("WARNING: WRONG WEIGHTS PATH!!!")
     seqlen = 32
-    input_size = 32 + 3  # latent_dim + action_dim
+    # input_size = 32 + 3  # latent_dim + action_dim
+    input_size = 32
+    print("WARNING: WRONG INPUT SIZE!!!")
     output_size = 32  # latent_dim + reward_dim
     model = AutoregressiveTransformer(
         _get_transformer_params(), output_size=output_size, num_components=5
--- scripts/one_offs/make_sure_transformer_is_not_constant.py
+++ scripts/one_offs/make_sure_transformer_is_not_constant.py
@@ -7,19 +7,20 @@ for x in encoded_rollouts.get_rollouts_ds(split="validation"):
 
 a = x["actions"][:, :3]
 o = x["observations"]
-inputs = tf.concat([o, a], axis=-1)
+# inputs = tf.concat([o, a], axis=-1)
+inputs = o
 inputs = tf.expand_dims(inputs, axis=0)
 
 model = saved_models.encoded_rollout_transformer()
 y = model(inputs[:, 300:332])
 var = tf.math.reduce_std(y, axis=-2)
 var = tf.squeeze(var)
-targets = o[1:] - o[:-1]
+# targets = o[1:] - o[:-1]
 
-mix = model.get_mix_of_gauss(y)
-print(-mix.log_prob([targets[200:232]]))
-print(-mix.log_prob([targets[20:52]]))
+# mix = model.get_mix_of_gauss(y)
+# print(-mix.log_prob([targets[200:232]]))
+# print(-mix.log_prob([targets[20:52]]))
 
-var = tf.math.reduce_std(targets[200:232], axis=-2)
-var = tf.squeeze(var)
-print(var)
+# var = tf.math.reduce_std(targets[200:232], axis=-2)
+# var = tf.squeeze(var)
+# print(var)

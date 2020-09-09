582e0820f167e6f7beb5b4ce652ad2d33ec67720
Michael Matena <michael.matena@gmail.com>
2020-09-09 11:20:33 -0400

    Default commit message.

--- scripts/util/gym_server.py
+++ scripts/util/gym_server.py
@@ -5,14 +5,14 @@ hostname = socket.gethostbyname(socket.gethostname())
 print(f"HOSTNAME: {hostname}")
 
 
-address = (hostname, 6000)  # family is deduced to be 'AF_INET'
-listener = connection.Listener(address, authkey=b"authkey")
-conn = listener.accept()
-print("connection accepted from", listener.last_accepted)
-while True:
-    msg = conn.recv()
-    # do something with msg
-    if msg == "close":
-        conn.close()
-        break
-listener.close()
+# address = (hostname, 6000)  # family is deduced to be 'AF_INET'
+# listener = connection.Listener(address, authkey=b"authkey")
+# conn = listener.accept()
+# print("connection accepted from", listener.last_accepted)
+# while True:
+#     msg = conn.recv()
+#     # do something with msg
+#     if msg == "close":
+#         conn.close()
+#         break
+# listener.close()

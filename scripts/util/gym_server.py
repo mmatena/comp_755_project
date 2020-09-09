585e1175b935229b169c220509e2ca55ed050288
Michael Matena <michael.matena@gmail.com>
2020-09-09 11:15:37 -0400

    Default commit message.

---
+++ scripts/util/gym_server.py
@@ -0,0 +1,18 @@
+from multiprocessing import connection
+import socket
+
+hostname = socket.gethostbyname(socket.gethostname())
+print(f"HOSTNAME: {hostname}")
+
+
+address = (hostname, 6000)  # family is deduced to be 'AF_INET'
+listener = connection.Listener(address, authkey=b"authkey")
+conn = listener.accept()
+print("connection accepted from", listener.last_accepted)
+while True:
+    msg = conn.recv()
+    # do something with msg
+    if msg == "close":
+        conn.close()
+        break
+listener.close()

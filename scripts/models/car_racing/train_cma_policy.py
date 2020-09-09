ff6ed79b35769715805910d213db222142216504
Michael Matena <michael.matena@gmail.com>
2020-09-09 13:27:15 -0400

    Default commit message.

--- scripts/models/car_racing/train_cma_policy.py
+++ scripts/models/car_racing/train_cma_policy.py
@@ -42,7 +42,7 @@ with open(IP_FILE, "r") as f:
     ip = f.read()
 
 # TODO(mmatena): Make this configurable.
-gym_service = rpyc.connect(ip, 18861).root
+gym_service = rpyc.connect(ip, 18861, config={"allow_all_attrs": True}).root
 
 encoder = saved_models.raw_rollout_vae_32ld()
 sequence_model = saved_models.encoded_rollout_transformer()

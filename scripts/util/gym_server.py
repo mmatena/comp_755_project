b8f1525ec35c27e8f0731a782697724dab8c8722
Michael Matena <michael.matena@gmail.com>
2020-09-10 14:35:29 -0400

    Default commit message.

--- scripts/util/gym_server.py
+++ scripts/util/gym_server.py
@@ -86,7 +86,7 @@ class GymEnvironments(multiprocessing.Process):
             #
             #
             env.viewer = 7
-            env.transform = 8
+            env.render = 8
             print(env.viewer)
             #
             #

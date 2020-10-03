cf4b1b3d01f3ec292313d66c62f5ffddedad0afd
Michael Matena <michael.matena@gmail.com>
2020-10-03 13:48:15 -0400

    Default commit message.

--- scripts/util/gym_server.py
+++ scripts/util/gym_server.py
@@ -64,8 +64,8 @@ class GymEnvironments(object):
 
         self.envs = [gym.make(self.env_name) for _ in range(self.num_environments)]
         for env in self.envs:
-            WINDOW_W = 1000
-            WINDOW_H = 800
+            WINDOW_W = 96
+            WINDOW_H = 96
             env.viewer = rendering.Viewer(
                 WINDOW_W, WINDOW_H, display=f":{self.display.display}"
             )

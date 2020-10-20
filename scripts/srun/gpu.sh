b875e1fb6adcb2fdddfab616676a360914cd7eb8
Michael Matena <michael.matena@gmail.com>
2020-10-20 14:36:07 -0400

    Default commit message.

--- scripts/srun/gpu.sh
+++ scripts/srun/gpu.sh
@@ -27,10 +27,10 @@ launch() {
   # Not too sure why I have to do it like this, but just running the comman
   # causes it fail to launch.
   CMD=$(echo srun \
-    --ntasks=12 \
+    --ntasks=4 \
     --time=2:30:00 \
-    --mem=8g \
-    --partition=volta-gpu \
+    --mem=4g \
+    --partition=gpu \
     --gres=gpu:1 \
     --qos=gpu_access \
     --pty \

d1603984c4e051069fc1e829c910d51b3ce8f938
Michael Matena <michael.matena@gmail.com>
2020-10-20 21:35:54 -0400

    Default commit message.

--- rl755/models/memory/trained/bigfish.py
+++ rl755/models/memory/trained/bigfish.py
@@ -12,7 +12,7 @@ def deterministic_transformer_32dm_32di():
     weights_path = "/pine/scr/m/m/mmatena/comp_755_project/models/memory/deterministic_transformer_32dm_32di/model-075.hdf5"
     model = instances.deterministic_transformer_32dm_32di()
     # Build the model.
-    model.build([None, 32, 32 + ACTION_SIZE])
+    model.build([32, 32 + ACTION_SIZE])
     # model(tf.zeros([1, 32, 32 + ACTION_SIZE]))
     model.load_weights(weights_path)
     return model

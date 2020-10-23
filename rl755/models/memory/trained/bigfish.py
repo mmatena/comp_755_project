891167fda6033bd6332b0e855014e1ccd996bd87
Michael Matena <michael.matena@gmail.com>
2020-10-23 17:48:37 -0400

    Default commit message.

--- rl755/models/memory/trained/bigfish.py
+++ rl755/models/memory/trained/bigfish.py
@@ -8,10 +8,10 @@ ACTION_SIZE = 15
 
 def deterministic_transformer_32dm_32di():
     # TODO: Add docs
-    weights_path = "/pine/scr/m/m/mmatena/comp_755_project/models/memory/bigfish/deterministic_transformer_32dm_32di/model-100.hdf5"
+    weights_path = "/pine/scr/m/m/mmatena/comp_755_project/models/memory/bigfish/deterministic_transformer_32dm_32di/model-005.hdf5"
     model = instances.deterministic_transformer_32dm_32di()
     # Build the model.
     print("TODO: Figure out how to do this without knowing batch or sequence length.")
-    model(tf.zeros([1024, 32, 32 + ACTION_SIZE]))
+    model(tf.zeros([16 * 8, 32, 32 + ACTION_SIZE]))
     model.load_weights(weights_path)
     return model

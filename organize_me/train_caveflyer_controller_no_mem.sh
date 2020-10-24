e2d1026b28770b59d1701ec80b3410063a804c93
Michael Matena <michael.matena@gmail.com>
2020-10-24 14:46:43 -0400

    Default commit message.

---
+++ organize_me/train_caveflyer_controller_no_mem.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#############################################################
+# Various variables to set.
+#############################################################
+# TODO(mmatena): Make these flags.
+
+# The directory of the cloned github repo.
+PROJECT_DIR=~/projects/comp_755_project
+
+MODEL_DIR=/pine/scr/m/m/mmatena/tmp/caveflyer_controller_no_mem_test
+
+NUM_CORES=16
+MEMORY=16g
+TIME="1-"
+#############################################################
+
+
+unset OMP_NUM_THREADS
+SIMG_PATH=/nas/longleaf/apps/tensorflow_py3/2.1.0/simg
+SIMG_NAME=tensorflow2.1.0-py3-cuda10.1-ubuntu18.04.simg
+
+module add python/3.6.6
+module add tensorflow_py3/2.1.0
+
+export PYTHONPATH=$PYTHONPATH:$PROJECT_DIR
+
+run_python() {
+  echo python $PROJECT_DIR/scripts/models/train_controller.py \
+    --environment=caveflyer \
+    --model_dir=$MODEL_DIR \
+    --memory_model=no_mem \
+    --vision_model=vae_32d \
+    --rollout_max_steps=1000 \
+    --cma_population_size=64 \
+    --cma_trials_per_member=16 \
+    --max_simul_envs=128 \
+    --cma_steps=1000
+}
+
+
+run_singularity() {
+  echo singularity exec --nv -B /pine -B /proj $SIMG_PATH/$SIMG_NAME bash -c "\\\"$(run_python)\\\""
+}
+
+launch() {
+  # Not too sure why I have to do it like this, but just running the command
+  # causes it fail to launch.
+  CMD=$(echo sbatch \
+    --ntasks=${NUM_CORES} \
+    --error="$MODEL_DIR/logs-%j.err" \
+    --output="$MODEL_DIR/logs-%j.out" \
+    --time=${TIME} \
+    --mem=${MEMORY} \
+    --partition=gpu \
+    --gres=gpu:1 \
+    --qos=gpu_access \
+    --wrap="\"$(run_singularity)\"")
+  eval $CMD
+}
+
+
+# Make the model directory if it does not exist.
+[ -d $MODEL_DIR ] || mkdir $MODEL_DIR
+# Run the command to actually launch the job.
+launch
---
+++ rl755/models/memory/trained/caveflyer.py
@@ -0,0 +1,21 @@
+"""Trained memory models for the caveflyer environment."""
+import tensorflow as tf
+
+from .. import instances
+
+ACTION_SIZE = 15
+
+
+def deterministic_transformer_32dm_32di():
+    # TODO: Add docs
+    weights_path = "/pine/scr/m/m/mmatena/comp_755_project/models/memory/caveflyer/deterministic_transformer_32dm_32di/model-18.hdf5"
+    model = instances.deterministic_transformer_32dm_32di()
+    # Build the model.
+    print("TODO: Figure out how to do this without knowing batch or sequence length.")
+    model(tf.zeros([128, 32, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def no_mem():
+    return instances.no_mem()

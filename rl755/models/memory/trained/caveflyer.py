8757d86909689f3a872fcb81c1b34e4f8ea2efee
Michael Matena <michael.matena@gmail.com>
2020-10-24 19:00:30 -0400

    Default commit message.

--- rl755/models/memory/trained/caveflyer.py
+++ rl755/models/memory/trained/caveflyer.py
@@ -17,5 +17,16 @@ def deterministic_transformer_32dm_32di():
     return model
 
 
+def deterministic_transformer_64dm_32di():
+    # TODO: Add docs
+    weights_path = "/pine/scr/m/m/mmatena/comp_755_project/models/memory/caveflyer/deterministic_transformer_64dm_32di/model-024.hdf5"
+    model = instances.deterministic_transformer_64dm_32di()
+    # Build the model.
+    print("TODO: Figure out how to do this without knowing batch or sequence length.")
+    model(tf.zeros([128, 32, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
 def no_mem():
     return instances.no_mem()

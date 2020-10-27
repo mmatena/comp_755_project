c2a8666790ae888e4d26a81e68b0954f383808a7
Michael Matena <michael.matena@gmail.com>
2020-10-26 22:38:29 -0400

    Default commit message.

--- organize_me/train_caveflyer_transformers_64.sh
+++ organize_me/train_caveflyer_transformers_64.sh
@@ -8,7 +8,7 @@
 # The directory of the cloned github repo.
 PROJECT_DIR=~/projects/comp_755_project
 
-MODEL=deterministic_transformer_64dm_32di_long
+MODEL=deterministic_transformer_64dm_32di_short
 MODEL_DIR=/pine/scr/m/m/mmatena/comp_755_project/models/memory/caveflyer/$MODEL
 
 TRAIN_STEPS=100000

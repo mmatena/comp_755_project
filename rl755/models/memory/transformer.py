f4b7452dfb1dd41e2d9ccc943400da354b0f8c0a
Michael Matena <michael.matena@gmail.com>
2020-10-28 13:06:23 -0400

    Default commit message.

--- rl755/models/memory/transformer.py
+++ rl755/models/memory/transformer.py
@@ -78,13 +78,6 @@ class ArTransformer(MemoryComponent):
             name=pos_embeddings_name,
         )
 
-        # self.initial_layer = tf.keras.layers.Dense(
-        #     units=transformer_params.hidden_size, activation=None, name="initial_dense"
-        # )
-        # self.final_layer = tf.keras.layers.Dense(
-        #     units=self.output_size, activation=None, name="final_dense"
-        # )
-
     def build(self, input_shape):
         hidden_size = self.transformer_params.hidden_size
 

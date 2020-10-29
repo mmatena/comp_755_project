5302cac691a457d3eab8a852f3cefd2352e70e8b
Michael Matena <michael.matena@gmail.com>
2020-10-29 12:10:53 -0400

    Default commit message.

--- rl755/models/memory/trained/caveflyer.py
+++ rl755/models/memory/trained/caveflyer.py
@@ -42,6 +42,78 @@ def deterministic_transformer_256dm_32di(**kwargs):
     return model
 
 
+def deterministic_transformer_64dm_32di_long(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_long", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_long(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def deterministic_transformer_64dm_32di_short(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_short", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_short(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def deterministic_transformer_64dm_32di_skinny(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_skinny", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_skinny(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def deterministic_transformer_64dm_32di_wide(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_wide", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_wide(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def deterministic_transformer_64dm_32di_short_skinny(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_short_skinny", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_short_skinny(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
+def deterministic_transformer_64dm_32di_long_wide(**kwargs):
+    # TODO: Add docs
+    weights_path = _WEIGHTS_PATTERN.format(
+        "deterministic_transformer_64dm_32di_long_wide", "100"
+    )
+    model = instances.deterministic_transformer_64dm_32di_long_wide(**kwargs)
+    # Build the model.
+    model(tf.keras.Input([None, 32 + ACTION_SIZE]))
+    model.load_weights(weights_path)
+    return model
+
+
 def deterministic_lstm_32dm_32di():
     # TODO: Add docs
     weights_path = _WEIGHTS_PATTERN.format("deterministic_lstm_32dm_32di", "100")

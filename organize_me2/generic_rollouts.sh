ff8f7ca601fed6527796190b8ad43fcf1ae258f8
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2020-11-03 10:02:10 -0500

    temporarily move folder

--- organize_me2/generic_rollouts.sh
+++ organize_me2/generic_rollouts.sh
@@ -1,10 +1,10 @@
 [ -d ../tmp/$env ] || mkdir ../tmp/$env
 [ -d ../tmp/$env/raw_rollouts ] || mkdir ../tmp/$env/raw_rollouts
 
-[ -d ../tmp/$env/raw_rollouts/test ] || mkdir ../tmp/$env/raw_rollouts/test
+[ -d ../tmp/$env/raw_rollouts/train ] || mkdir ../tmp/$env/raw_rollouts/train
 python scripts/data_gen/create_rollouts.py \
-    --out_dir=../tmp/$env/raw_rollouts/test \
-    --num_rollouts=1000 \
+    --out_dir=../tmp/$env/raw_rollouts/train \
+    --num_rollouts=5000 \
     --environment=$env \
     --max_steps=1000 \
     --max_simul_envs=64
---
+++ rorganize_me/test.py
@@ -0,0 +1,6 @@
+import os
+from pydoc import locate
+
+from absl import app
+from absl import flags
+import tensorflow as tf

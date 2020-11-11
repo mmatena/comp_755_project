bbe51303ec7259c99c86a9ab9801c2744cc72543
Michael Matena <michael.matena@gmail.com>
2020-11-11 13:14:38 -0500

    Default commit message.

--- rl755/models/vision/simclr.py
+++ rl755/models/vision/simclr.py
@@ -35,6 +35,7 @@ def _get_encoder(representation_size):
 def _get_head(representation_size):
     return tf.keras.Sequential(
         [
+            tf.keras.layers.Dense(units=64, activation="relu"),
             tf.keras.layers.Dense(units=16),
         ]
     )
